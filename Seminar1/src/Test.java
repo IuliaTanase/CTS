55b2ca2b88df3d093bac898be0c5d7e853f15665
Iulia Tanase <iuliatanase1998@gmail.com>
2021-02-27 12:58:51 +0200

    first branch

--- Seminar1/src/Test.java
+++ Seminar1/src/Test.java
@@ -2,5 +2,8 @@
 public class Test {
 	public static void main(String args[]) {
 		System.out.println("Hello World!");
-	}
+		
+		Persoana p1 = new Persoana("Chuck", "Norris");
+		System.out.println(p1.toString());
+	}	
 }
