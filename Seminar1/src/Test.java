8e9ab778ca7b3ca25c9e10907db5e4eb64d0e58b
Iulia Tanase <iuliatanase1998@gmail.com>
2021-02-27 13:03:13 +0200

    added person

--- Seminar1/src/Test.java
+++ Seminar1/src/Test.java
@@ -2,5 +2,8 @@
 public class Test {
 	public static void main(String args[]) {
 		System.out.println("Hello World!");
+		
+		Person person = new Person("Chuck", "Norris");
+		System.out.println(person.toString());
 	}
 }
