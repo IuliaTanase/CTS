3cd24a38b46075088b954e90cb1d6e5163b93b90
Iulia Tanase <iuliatanase1998@gmail.com>
2021-03-04 15:29:45 +0200

    added fee bank account

--- Seminar1/src/ro/ase/csie/cts/seminar2/FeeBankAccount.java
+++ Seminar1/src/ro/ase/csie/cts/seminar2/FeeBankAccount.java
@@ -2,6 +2,7 @@ package ro.ase.csie.cts.seminar2;
 
 public class FeeBankAccount extends BankAccount {
 
+	//OPEN CLOSED PRINCIPLE 
 	private long fee = 2; //comisionul
 	public FeeBankAccount(String iban, Person person) {
 		super(iban, person);
