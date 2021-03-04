5cb64c47383cf1c979ae3194a2fdedc4cbfd40c2
Iulia Tanase <iuliatanase1998@gmail.com>
2021-03-04 14:17:26 +0200

    added validation on weekDay3 function

--- Seminar1/src/ro/ase/csie/cts/seminar2/CalendarUtil.java
+++ Seminar1/src/ro/ase/csie/cts/seminar2/CalendarUtil.java
@@ -44,6 +44,11 @@ public class CalendarUtil {
 	
 	public String weekDay3(int day) {
 		String days[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+		
+		if(day < 0 || day > days.length) {
+			return null;
+		}
+		
 		return days[day - 1];
 		
 	}
