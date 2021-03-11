c47dead782722d60e13ea0f84272222a4ee1cd3d
Iulia Tanase <iuliatanase1998@gmail.com>
2021-03-11 15:19:41 +0200

    refactorings

---
+++ Seminar1/src/ro/ase/csie/cts/seminar1/Test.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.cts.seminar1;
+
+public class Test {
+	public static void main(String args[]) {
+		System.out.println("Hello World!");
+		
+		Person person = new Person("Chuck", "Norris");
+		Person secondPerson = new Person("Vin", "Diesel");
+		System.out.println(person.toString());
+		System.out.println(secondPerson.toString());
+	}
+}
