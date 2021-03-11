1839ae05de701ec372ae287a83932fca45dddb7f
Iulia Tanase <iuliatanase1998@gmail.com>
2021-03-11 14:17:47 +0200

    multiple account interfaces

--- Seminar1/src/ro/ase/csie/cts/seminar3/Payable.java
+++ Seminar1/src/ro/ase/csie/cts/seminar3/Payable.java
@@ -1,6 +1,5 @@
 package ro.ase.csie.cts.seminar3;
 
-public interface Account {
-	void deposit(long amount);
+public interface Payable {
 	void withdraw(long amount) throws InsufficientFundsException;
 }
