d246f812d956a2ede08de30906489e75a59671be
Iulia Tanase <iuliatanase1998@gmail.com>
2021-03-31 15:35:57 +0300

    finished builder & prototype dp

--- Seminarii/src/ro/ase/csie/cts/seminar6/builder/SuperErou.java
+++ Seminarii/src/ro/ase/csie/cts/seminar6/builder/SuperErou.java
@@ -1,12 +1,11 @@
 package ro.ase.csie.cts.seminar6.builder;
 
 public class SuperErou {
+	private String nume;
+	private int puncteViata;
 	
-	String nume;
-	int puncteViata;
-	
-	boolean esteNegativ;
-	boolean esteRanitGrav;
+	private boolean esteNegativ;
+	private boolean esteRanitGrav;
 	
 	InterfataArma armaStanga;
 	InterfataArma armaDreapta;
@@ -14,11 +13,11 @@ public class SuperErou {
 	InterfataSuperPutere superSuperPutere;
 	
 	//permite dezvoltatorilor sa construiasca super eroi => constructori
-	public SuperErou() {
+	private SuperErou() {
 		
 	}
 
-	public SuperErou(String nume, int puncteViata, boolean esteNegativ, boolean esteRanitGrav, InterfataArma armaStanga,
+	private SuperErou(String nume, int puncteViata, boolean esteNegativ, boolean esteRanitGrav, InterfataArma armaStanga,
 			InterfataArma armaDreapta, InterfataSuperPutere superPutere, InterfataSuperPutere superSuperPutere) {
 		super();
 		this.nume = nume;
@@ -31,5 +30,53 @@ public class SuperErou {
 		this.superSuperPutere = superSuperPutere;
 	}
 	
-	
+	//definire builder
+	//static pt a putea accesa builderul fara sa construiesc un obiect de tip SuperErou
+	public static class SuperErouBuilder {
+		SuperErou superErou;
+		
+		//in constructor punem atributele care sunt absolut necesare pt a crea un supererou
+		public SuperErouBuilder(String nume, int puncteViata) {
+			this.superErou = new SuperErou();
+			this.superErou.nume = nume;
+			this.superErou.puncteViata = puncteViata;
+		}
+		
+		//definire metode care permit dezv sa schimbe celelalte atribute ale supereroului
+		//intorc referinta catre BUILDER
+		public SuperErouBuilder esteNegativ() {
+			this.superErou.esteNegativ = true;
+			return this;
+		}
+		
+		public SuperErouBuilder esteRanit() {
+			this.superErou.esteRanitGrav = true;
+			return this;
+		}
+		
+		public SuperErouBuilder setArmaStanga(InterfataArma arma) {
+			this.superErou.armaStanga = arma;
+			return this;
+		}
+		
+		public SuperErouBuilder setArmaDreapta(InterfataArma arma) {
+			this.superErou.armaDreapta = arma;
+			return this;
+		}
+		
+		public SuperErouBuilder setSuperPutere(InterfataSuperPutere superPutere) {
+			this.superErou.superPutere = superPutere;
+			return this;
+		}
+		
+		public SuperErouBuilder setSuperSuperPutere(InterfataSuperPutere superSuperPutere) {
+			this.superErou.superSuperPutere = superSuperPutere;
+			return this;
+		}
+		
+		public SuperErou build() {
+			return this.superErou;
+		}
+	}
+		
 }
--- Seminarii/src/ro/ase/csie/cts/seminar6/builder/TestBuilder.java
+++ Seminarii/src/ro/ase/csie/cts/seminar6/builder/TestBuilder.java
@@ -1,21 +1,40 @@
 package ro.ase.csie.cts.seminar6.builder;
 
-public class TestBuilder {
 
-	public static void main(String[] args) {
-		
-		//I.
-		//1. construire obiect
-		SuperErou superErou = new SuperErou(); //=> supereroii nu vor avea detalii (toate sunt null sau 0)
-		
-		//2. trb initializate atributele obiectului
-		superErou.nume = "Superman";
-		
-		//II.
-		SuperErou superErou2 = new SuperErou("Batman", 100, false, false, new Arma(), null, null, null); //nu e clean
-		
-		//III. BUILDER
-		
+	public class TestBuilder {
+
+		public static void main(String[] args) {
+			
+			//I.
+			//1. construire obiect
+//			SuperErou superErou = new SuperErou(); //=> supereroii nu vor avea detalii (toate sunt null sau 0)
+			
+			//2. trb initializate atributele obiectului
+//			superErou.nume = "Superman";
+			
+			//II.
+//			SuperErou superErou2 = new SuperErou("Batman", 100, false, false, new Arma(), null, null, null); //nu e clean
+			
+			//III. BUILDER
+			//constructorii privati + implementare builder din SuperErou
+			SuperErou superman = new SuperErou.SuperErouBuilder("Superman", 100)	//daca vreau sa fac configurari, le fac prin metodele definite, inainte de a apela build
+					.setArmaDreapta(new Arma())
+					.setArmaStanga(new Arma())
+					.build(); //obtin supereroul
+			
+			SuperErou joker = new SuperErou.SuperErouBuilder("Joker", 200)
+					.esteNegativ()
+					.esteRanit()
+					.setArmaDreapta(new Arma())
+					.build();
+			
+			//daca vreau sa mi mai construiesc un joker, ma pot folosi de un dictionar pt a nu face copy-paste
+			SuperErou joker2 = new SuperErou.SuperErouBuilder("Joker", 200)
+					.esteNegativ()
+					.esteRanit()
+					.setArmaDreapta(new Arma())
+					.build();
+		}
+
 	}
 
-}
