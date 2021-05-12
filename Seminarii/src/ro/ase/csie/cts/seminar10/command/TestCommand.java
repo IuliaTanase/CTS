a1832113afd1f3f0a60ce9e361022cc34b17c2e5
Iulia Tanase <iuliatanase1998@gmail.com>
2021-05-12 14:24:52 +0300

    added manager for command pattern

--- Seminarii/src/ro/ase/csie/cts/seminar10/command/TestCommand.java
+++ Seminarii/src/ro/ase/csie/cts/seminar10/command/TestCommand.java
@@ -8,7 +8,18 @@ public class TestCommand {
 		TaskAsincron autoUpdate = new TaskToDo(new ModulUpdateClient(), "Update v2", 20);
 		
 		//un alt thread va parcurge lista si va executa anumite taskuri
-		autoSave.executaTask();
-		autoUpdate.executaTask();
+		//autoSave.executaTask();
+		//autoUpdate.executaTask();
+		
+		ManagerTaskuri manager = new ManagerTaskuri();
+		manager.adaugaTask(autoSave);
+		manager.adaugaTask(autoUpdate);
+		
+		System.out.println("Jocul este activ.\n");
+		
+		manager.executaTask();
+		manager.executaTask();
+		
+		System.out.println("\nJocul este activ.\n");
 	}
 }
